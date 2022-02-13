b06bbae379482296eb52353869ad19314b1aef2a
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-02-13 13:22:23 +0300

    Added the quotes to the index page

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,10 @@
 from flask import render_template
 from . import main
+from ..requests import random
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    
+    random_quote = random()
+
+    return render_template('index.html', random = random_quote)
