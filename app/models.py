49832b3c667588baed937fee66b079f9188e333f
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-02-13 15:18:13 +0300

    Changed table name from writers to users

--- app/models.py
+++ app/models.py
@@ -1,5 +1,11 @@
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class Quotes:
     '''
@@ -9,11 +15,12 @@ class Quotes:
         self.author = author
         self.quote = quote
 
-class Writer(db.Model):
-    __tablename__ = 'writers'
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(255))
+    username = db.Column(db.String(255), index = True)
+    email = db.Column(db.String(255), unique = True)
     pass_secure = db.Column(db.String(255))
 
     @property
