6fcaf2985c3cf2e1ce3e0bf20d3d597f47cc577b
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-02-13 21:41:04 +0300

    Initialized ckeditor

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,9 @@ from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
+from flask_ckeditor import CKEditor
 
 
 bootstrap = Bootstrap()
@@ -10,6 +13,9 @@ db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+photos = UploadSet('photos',IMAGES)
+mail = Mail()
+ckeditor = CKEditor()
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -21,6 +27,8 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    mail.init_app(app)
+    ckeditor.init_app(app)
 
     #Registering main blueprint
     from .main import main as main_blueprint
@@ -32,5 +40,8 @@ def create_app(config_name):
     #setting config
     from .requests import configure_request
     configure_request(app)
+
+    #configure UploadSet
+    configure_uploads(app,photos)
 
     return app
