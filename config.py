d36f3b63f01f5714fb171bb4d613a215df9026da
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-02-13 21:41:32 +0300

    Created a configuration for testing

--- config.py
+++ config.py
@@ -10,6 +10,14 @@ class Config:
     QUOTES_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:R0707318659@localhost/blog'
     SECRET_KEY = os.environ.get('SECRET_KEY') 
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+     #email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 class ProdConfig(Config):
     '''
@@ -19,6 +27,10 @@ class ProdConfig(Config):
       Config: The parent configuration class with General configuration settings
     '''
     pass
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:R0707318659@localhost/blog_test'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:R0707318659@localhost/comments_test'
 
 class DevConfig(Config):
     '''
@@ -31,5 +43,6 @@ class DevConfig(Config):
 
 config_options = {
   'development': DevConfig,
-  'production': ProdConfig
+  'production': ProdConfig,
+  'test': TestConfig
 }    
