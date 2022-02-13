be77999d80e8ac337030fe7a104d5bea5b437827
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-02-13 14:06:17 +0300

    Created a writer tables

--- app/models.py
+++ app/models.py
@@ -1,3 +1,6 @@
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
 class Quotes:
     '''
     Source class to define quote objects
@@ -5,3 +8,24 @@ class Quotes:
     def __init__(self, author, quote):
         self.author = author
         self.quote = quote
+
+class Writer(db.Model):
+    __tablename__ = 'writers'
+
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password) :
+        self.pass_secure = generate_password_hash(password) 
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure, password)
+
+    def __repr__(self):
+        return f'Writer{self.username}'
