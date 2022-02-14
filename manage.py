50a3250554fd3ad0d0c03084dbf22251e185da80
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-02-14 05:20:36 +0300

    Removed unnecessary code

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ from flask_script import Manager, Server
 from app.models import User
 from flask_migrate import Migrate, MigrateCommand
 
-app = create_app('test')
+app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server', Server)
@@ -21,8 +21,6 @@ def test():
 @manager.shell
 def make_shell_context():
     return dict(app = app,db = db, User = User )
-if __name__ == '__main__':
-    manager.run()
 
 if __name__ == '__main__':
     manager.run()
